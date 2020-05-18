b1945dae7b018bfc2297e0e6a0dcd0043adbaed9
airtonfilho04 <airtonfilhocontato@outlook.com>
2020-05-18 09:47:17 -0300

    Aula 75

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+coin = [
+    {
+      description: "Bitcoin",
+      acronym: "BTC",
+      url_image:"https://s2.glbimg.com/gfP8fLw2BQk95QMfzXGtjDdjn88=/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_63b422c2caee4269b8b34177e8876b93/internal_photos/bs/2019/u/l/B3RPBlS12Za4gj8dBvCw/bitcoin.png"
+    },
+    {
+      description: "Etherium",
+      acronym: "ETH",
+      url_image:"https://s2.coinmarketcap.com/static/img/coins/200x200/1027.png"
+    }
+  ]
+
+coin.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
